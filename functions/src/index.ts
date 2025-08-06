45391e4f27d4598d115b31848817742859add116
Laurence2013 <lozza244@protonmail.com>
2025-08-06 08:09:59 +0100

    02commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,39 @@
 import {setGlobalOptions} from "firebase-functions";
 import {onRequest} from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
+
 import axios from "axios";
 
+import Stripe from "stripe";
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+	throw new Error("Stripe secret key is not set in environment variables");
+}
+
+const stripe00 = new Stripe(stripeSecretKey, {apiVersion: "2025-07-30.basil"});
+
 setGlobalOptions({maxInstances: 10});
+
+export const stripe01 = onRequest(async (req, res) => {
+	const plan = req.query.plan;
+
+	if (!plan) return res.send("Subscription plan not found");
+
+	let priceId;
+
+	switch (plan.toLowerCase()) {
+		case "Quotes in a jar x3":
+			priceId = "prod_SoUSRWjqzZNL4T";
+			break
+	}
+
+	const session = await stripe00.checkout.sessions.create({
+		mode: "subscription", 
+		line_items: []
+	});
+});
 
 export const helloWorld = onRequest((request, response) => {
 	logger.info("Hello logs!", {structuredData: true});
